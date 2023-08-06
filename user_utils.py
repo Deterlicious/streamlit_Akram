661bd2a43f77195426b62972f8686a0f424128c4
Aziz Nur Ashidiq <116855688+Deterlicious@users.noreply.github.com>
2023-08-06 07:39:57 -0700

    Update user_utils.py

--- user_utils.py
+++ user_utils.py
@@ -1,5 +1,7 @@
 import hashlib
 import json
+import pandas as pd
+import os
 
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
@@ -18,8 +20,17 @@ def save_users(users):
 
 def login(user, password):
     users = load_users()
-    if user in users and users[user] == hash_password(password):
-        return True
+    if user in users:
+        user_data = users[user]
+        # Jika password disimpan sebagai dictionary, ambil password dari dictionary
+        if isinstance(user_data, dict) and 'password' in user_data:
+            stored_password = user_data['password']
+        # Jika password disimpan sebagai string, gunakan string tersebut
+        elif isinstance(user_data, str):
+            stored_password = user_data
+        else:
+            return False
+        return stored_password == hash_password(password)
     return False
 
 def register(user, password):
@@ -28,20 +39,30 @@ def register(user, password):
         return False
     users[user] = hash_password(password)
     save_users(users)
+    # Buat file profil untuk pengguna baru
+    profile = pd.DataFrame([['', '', '', '']], columns=['Nama', 'Email', 'Alamat', 'No HP'])
+    save_profile(user, profile)
     return True
 
-def update_profile(user, name, email, address, phone, loan_duration):
-    users = load_users()
-    if user in users:
-        users[user] = {'password': users[user], 'name': name, 'email': email, 'address': address, 'phone': phone, 'loan_duration': loan_duration.strftime('%Y-%m-%d')}
-        save_users(users)
-        return True
-    return False
+def save_profile(username, df):
+    df.to_csv(f'{username}_profile.csv', index=False)
 
-def load_profile(user):
-    users = load_users()
-    if user in users:
-        profile = users[user]
-        profile['loan_duration'] = pd.to_datetime(profile['loan_duration'])
-        return profile
-    return None
+def update_profile(username, name, email, address, phone):
+    df = load_profile(username)
+    if df.empty:
+        # Jika DataFrame kosong, tambahkan baris baru
+        df.loc[0] = [name, email, address, phone]
+    else:
+        df.loc[0, 'Nama'] = name
+        df.loc[0, 'Email'] = email
+        df.loc[0, 'Alamat'] = address
+        df.loc[0, 'No HP'] = phone
+    save_profile(username, df)
+    return True
+
+def load_profile(username):
+    if os.path.exists(f'{username}_profile.csv'):
+        return pd.read_csv(f'{username}_profile.csv')
+    else:
+        # Jika file profil pengguna tidak ada, kembalikan DataFrame dengan satu baris kosong
+        return pd.DataFrame([['', '', '', '']], columns=['Nama', 'Email', 'Alamat', 'No HP'])
